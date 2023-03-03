dbffe42de6d85366c7bb9fc20c1428caa692e171
kangtoe <kangtoe99@gmail.com>
2023-03-03 23:00:59 +0900

    패턴 프리팹 수정
    -하나의 패턴은 사이즈 2인 grid의 12칸 (24) 너비에서 정상동작하도록 수정

--- Assets/Scripts/RepeatObjects.cs
+++ Assets/Scripts/RepeatObjects.cs
@@ -31,7 +31,7 @@ public class RepeatObjects : MonoBehaviour
 
             if (go.transform.position.x <= -width)
             {
-                Vector2 offset = new Vector2(width * 2f, 0f);
+                Vector2 offset = new Vector2(width * 2, 0f);
                 go.transform.position = (Vector2)go.transform.position + offset;
             }
         }
