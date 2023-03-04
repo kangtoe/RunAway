0059cd6d20e5477657e4cded926c2474617b7626
kangtoe <kangtoe99@gmail.com>
2023-03-04 09:05:23 +0900

    장애물 패턴은 게임 플레이 중에만 등장함

--- Assets/Scripts/Gound&Wall/RepeatGround.cs
+++ Assets/Scripts/Gound&Wall/RepeatGround.cs
@@ -18,16 +18,19 @@ public class RepeatGround : RepeatObjects
 
             if (objects[i].transform.position.x <= -width)
             {
-                Debug.Log("set ground to new pos");
+                //Debug.Log("set ground to new pos");
 
                 Vector2 offset = new Vector2(width * 2, 0f);
                 objects[i].transform.position = (Vector2)objects[i].transform.position + offset;
 
-                // 그라운드 패턴 교체
-                GameObject nextGround = Instantiate(GetRandomGround(), transform);
-                nextGround.transform.position = objects[i].transform.position;
-                Destroy(objects[i]);
-                objects[i] = nextGround;
+                if (GameManager.Instance.IsPlaying)
+                {
+                    // 그라운드 패턴 교체
+                    GameObject nextGround = Instantiate(GetRandomGround(), transform);
+                    nextGround.transform.position = objects[i].transform.position;
+                    Destroy(objects[i]);
+                    objects[i] = nextGround;
+                }                
             }
         }
     }
